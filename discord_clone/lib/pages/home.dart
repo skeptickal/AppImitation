e88e3d5475403789967946b22237d73313aa9d66
Jackson Geer <jacksongeer@gmail.com>
2023-10-03 20:38:09 -0400

    began discord clone home page

--- discord_clone/lib/pages/home.dart
+++ discord_clone/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePageScreen extends StatelessWidget {
   const HomePageScreen({super.key});
@@ -6,6 +7,27 @@ class HomePageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF36393e),
+        title:
+            const Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Row(children: [
+            Icon(Icons.tag),
+            SizedBox(width: 5),
+            Text(
+              'gaming',
+              style: TextStyle(fontWeight: FontWeight.normal, fontSize: 18),
+            ),
+          ]),
+          Row(
+            children: [
+              Icon(Icons.search),
+              Icon(Icons.people),
+            ],
+          ),
+        ]),
+      ),
+      drawer: Drawer(),
       body: Container(
         decoration: BoxDecoration(color: Color(0xFF424549)),
         child: Column(
